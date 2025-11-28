b6518d0f9280e61500810ecef528ca32fc27c4aa
loosebroose <kevindesouza@protonmail.com>
2025-11-28 08:53:21 -0800

    Latest mines

--- mine_name_updater.py
+++ mine_name_updater.py
@@ -4,16 +4,32 @@ import arcpy
 from os.path import exists
 import json
 from configparser import ConfigParser
+from os import environ
+import sys
 
+if "JENKINS_URL" in environ:
+    print("Running in Jenkins environment... using script tool inputs for credentials.")
+    oracle_username = sys.argv[1]  # bcgw username
+    oracle_password = sys.argv[2]  # bcgw password
+    agol_username = sys.argv[3]  # arcgis online username
+    agol_password = sys.argv[4]  # arcgis online password
 
-ini_file = r"H:\Profile\myfile.ini"
-if exists(ini_file):
-    config = ConfigParser()
-    config.read(ini_file)
-    oracle_username = config["credentials"]["bcgw_username"]
-    oracle_password = config["credentials"]["bcgw_password"]
-    agol_username = config["credentials"]["agol_username"]
-    agol_password = config["credentials"]["agol_password"]
+else:
+    ini_file = r"H:\Profile\myfile.ini"
+    try:
+        config = ConfigParser()
+        config.read(ini_file)
+        oracle_username = config["credentials"]["bcgw_username"]
+        oracle_password = config["credentials"]["bcgw_password"]
+        agol_username = config["credentials"]["agol_username"]
+        agol_password = config["credentials"]["agol_password"]
+    except:
+        arcpy.AddMessage("Reading from credentials .ini failed, using script tool inputs instead....")
+        oracle_username = arcpy.GetParameterAsText(0)
+        oracle_password = arcpy.GetParameterAsText(1)
+        agol_username = arcpy.GetParameterAsText(2)
+        agol_password = arcpy.GetParameterAsText(3)
+
 
 arcpy.SignInToPortal("https://governmentofbc.maps.arcgis.com", agol_username, agol_password)
 gis = GIS("https://governmentofbc.maps.arcgis.com", agol_username, agol_password, verify_cert=False)
